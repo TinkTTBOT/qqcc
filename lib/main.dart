f805fb6df807ce4ed15429d0d4dfddedb58b7d1f
TinkTTBOT <104524369+TinkTTBOT@users.noreply.github.com>
2025-12-07 19:07:07 +0700

    Set up initial Flutter app with MyApp widget
    Initialize Flutter app with basic structure and UI.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
 
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Dino TFT Builder")),
+        body: Center(child: Text("App đang được khởi tạo...")),
+      ),
+    );
+  
+  }
+}
